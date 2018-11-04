4ef1f59dd9ea39eb121eb1ebfb2670d9232c0923
LAPTOP-RJ1A082F\Moudi <moudisoubra001@gmail.com>
2018-11-04 14:07:06 +0400

    Added Map Generator

---
+++ Prototype 1/Assets/FailSafe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailSafe : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Debug.Log("This Shit is doing it");
+    }
+}
---
+++ Prototype 1/Assets/RoomSpawner.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour {
+
+    public int roomKind;
+
+    public RoomsArrays rooms;
+    public int randomRoom;
+    public bool spawned;
+    public float time = 5f;
+    public int limitStartingRooms;
+    public int maxLimiter;
+
+    private void Update()
+    {
+        if (rooms.startTime > 0 || rooms.roomAmount.Count <= 20)
+        {
+            limitStartingRooms = 2;
+            maxLimiter = 1;
+        }
+        else
+        {
+            limitStartingRooms = 0;
+            maxLimiter = 0;
+        }
+    }
+    private void Start()
+    {
+        
+        Destroy(gameObject, time);
+        rooms = FindObjectOfType<RoomsArrays>();
+
+        Invoke("SpawnRoom", 0.1f);
+    }
+    void SpawnRoom ()
+    {
+        if (!spawned)
+        {
+            if (roomKind == 1)
+            {
+                randomRoom = Random.Range(limitStartingRooms, rooms.roomsKindRight.Length);
+                Instantiate(rooms.roomsKindRight[randomRoom * maxLimiter], transform.position, rooms.roomsKindRight[randomRoom].transform.rotation);
+            }
+            else if (roomKind == 2)
+            {
+                randomRoom = Random.Range(limitStartingRooms, rooms.roomsKindBottom.Length);
+                Instantiate(rooms.roomsKindBottom[randomRoom * maxLimiter], transform.position, rooms.roomsKindBottom[randomRoom].transform.rotation);
+            }
+            else if (roomKind == 3)
+            {
+                randomRoom = Random.Range(limitStartingRooms, rooms.roomsKindTop.Length);
+                Instantiate(rooms.roomsKindTop[randomRoom * maxLimiter], transform.position, rooms.roomsKindTop[randomRoom].transform.rotation);
+            }
+            else if (roomKind == 4)
+            {
+                randomRoom = Random.Range(limitStartingRooms, rooms.roomsKindLeft.Length);
+                Instantiate(rooms.roomsKindLeft[randomRoom * maxLimiter], transform.position, rooms.roomsKindLeft[randomRoom].transform.rotation);
+            }
+            spawned = true;
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ("SpawnPoints"))
+        {
+                if (other.GetComponent<RoomSpawner>().spawned == false && spawned == false && other.gameObject != null)
+                {
+                    Instantiate(rooms.Block, transform.position, Quaternion.identity);
+                    Destroy(gameObject);
+                }
+                spawned = true;
+            
+        }
+    }
+}
