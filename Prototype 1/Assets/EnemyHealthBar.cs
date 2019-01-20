a32fef80d3cdd379f20267eb6cb7c5de0006e97a
Aniviaa <xkarimw@gmail.com>
2019-01-20 16:16:41 +0400

    Final Update 2

---
+++ Prototype 1/Assets/EnemyHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+
+    public EnemyScript enemyScript;
+
+    public float enemyHealth;
+
+    public Image enemyHealthBar;
+
+    public GameObject cameraMain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraMain = GameObject.Find("Main Camera");
+        enemyScript = GetComponentInParent<EnemyScript>();
+        enemyHealth = enemyScript.startHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyHealthBar.fillAmount = enemyScript.enemyHealth / enemyHealth;
+        transform.LookAt(cameraMain.transform.position);
+    }
+}
---
+++ Prototype 1/Assets/Scripts/checkDistancePlayerStone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkDistancePlayerStone : MonoBehaviour
+{
+    public PlayerController playerScript;
+    public GameObject player;
+    public float minDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScript = FindObjectOfType<PlayerController>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= minDistance)
+        {
+            playerScript.inStoneRange = true;
+        }
+        else
+        {
+            playerScript.inStoneRange = false;
+        }
+    }
+}
